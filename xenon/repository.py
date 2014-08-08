59a23243fa776c92c3259c7c187e79cbed5cfa82
Michele Lacchia <michelelacchia@gmail.com>
2014-08-08 18:00:49 +0200

    More information in git data

--- xenon/repository.py
+++ xenon/repository.py
@@ -1,14 +1,14 @@
 import os
 import sh
 
-FORMAT = '%n'.join(['%H', '%aN', '%ae', '%cN', '%ce', '%s'])
+FORMAT = '%n'.join(['%H', '%aN', '%ae', '%at', '%cN', '%ce', '%ct', '%s'])
 
 
 def gitrepo(root):
     oldpwd = sh.pwd().strip()
     sh.cd(root)
     gitlog = sh.git('--no-pager', 'log', '-1',
-                    pretty="format:%s" % FORMAT).split('\n', 5)
+                    pretty="format:%s" % FORMAT).split('\n', 7)
     branch = (os.environ.get('CIRCLE_BRANCH') or
               os.environ.get('TRAVIS_BRANCH',
                              sh.git('rev-parse',
@@ -21,9 +21,11 @@ def gitrepo(root):
             "id": gitlog[0],
             "author_name": gitlog[1],
             "author_email": gitlog[2],
-            "committer_name": gitlog[3],
-            "committer_email": gitlog[4],
-            "message": gitlog[5].strip(),
+            "author_timestamp": gitlog[3],
+            "committer_name": gitlog[4],
+            "committer_email": gitlog[5],
+            "committer_timestamp": gitlog[6],
+            "message": gitlog[7].strip(),
         },
         "branch": branch,
         "remotes": [{'name': r[0], 'url': r[1]} for r in remotes]
