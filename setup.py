2380c703ad438fe0c8b708c5226e1dd4e0749649
Michele Lacchia <michelelacchia@gmail.com>
2015-01-29 09:57:35 +0100

    chore(deps): upgrade radon dep

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='xenon',
       long_description=readme,
       packages=['xenon'],
       tests_require=['tox'],
-      install_requires=['radon==1.0', 'requests', 'sh', 'PyYAML'],
+      install_requires=['radon', 'requests', 'sh', 'PyYAML'],
       entry_points={'console_scripts': ['xenon = xenon:main']},
       classifiers=[
           'Development Status :: 4 - Beta',
