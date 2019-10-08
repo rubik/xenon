a5d5e43dbc226301a423e1522d61f090ca154c8e
Dave Gallant <davegallant@gmail.com>
2019-10-08 12:22:41 -0400

    feat: allow xenon to be run as a module (#33)

---
+++ xenon/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""The main entry point. Invoke as `xenon' or `python -m xenon'.
+"""
+from . import main
+
+
+def entrypoint():
+    main()
+
+
+if __name__ == "__main__":
+    main()
